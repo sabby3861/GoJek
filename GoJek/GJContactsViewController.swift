e2874191d6e70c4e9b068e2ca7f0e4f2bd480c32
Sanjay Chauhan <sanjay.kumar@allianz.co.id>
2018-08-11 12:38:50 +0700

    . Prefix added for class name . Embed Navigation Controller

--- GoJek/GJContactsViewController.swift
+++ GoJek/GJContactsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GJContactsViewController.swift
 //  GoJek
 //
 //  Created by sanjay on 11/08/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GJContactsViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
