babe4e8e9f8201a50ffc2abbd61d9a9d7f99b3c7
Sanjay Chauhan <sanjay.kumar@allianz.co.id>
2018-08-14 02:05:41 +0700

    . Added one test case.. Lack of time

--- GoJekTests/GoJekTests.swift
+++ GoJekTests/GoJekTests.swift
@@ -7,30 +7,47 @@
 //
 
 import XCTest
+
 @testable import GoJek
 
 class GoJekTests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testContacts() {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let parser = GJJSONParser()
+    let service = GJContactService()
+    parser.request(resource: service.service) { result in
+      switch result {
+      case .success(let data):
+        print("Data is \(data)")
+        XCTAssertFalse(data.isEmpty)
+      case .failure(let missing):
+        let error = missing.localizedDescription
+        print("Description  \(error)")
+        XCTFail(error)
+      }
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+  }
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
 }
